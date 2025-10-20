ba914afa2fe79f5394e00f68890dc94f824b3bc7
Tenebris An <y2222672@gmail.com>
2025-10-20 20:32:09 +0300

    add initiales files

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,3 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
-  return ` You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
-
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
   if (totalPrice > customerCredits) {
