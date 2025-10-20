c4521016f7acb40bbb3cf2a3c0e4ed8ab4c09a3a
Tenebris An <y2222672@gmail.com>
2025-10-20 16:20:16 +0300

    add initiales files

--- js/task-3.js
+++ js/task-3.js
@@ -10,3 +10,20 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
+
+function checkForSpam(message) {
+const lowerMessage = message.toLowerCase();
+return lowerMessage.includes("spam") || lowerMessage.includes("sale");
+} 
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+
+
